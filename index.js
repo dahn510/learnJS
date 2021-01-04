c752859ae17820fc8348116c990246f2533107a0
daniel <dahn510@gmail.com>
2021-01-04 20:36:58 +0000

    I learned if/else statement

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ function logResize()
 {
     let elem = document.getElementById("bob");
     click_count++;
-    elem.innerHTML = "You clicked: " + click_count;
+    if(click_count > 50){
+        elem.innerHTML = "Yamete oniisan, you clicked too much!!";
+    }else{
+        elem.innerHTML = "You clicked: " + click_count;
+    }
 }
 
 window.addEventListener("click",logResize);
